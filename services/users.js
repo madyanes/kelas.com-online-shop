a9a90ac79d83f7fdad6cdf5b6f3ada02a296963e
madyanes <madyanes.dev@gmail.com>
2023-10-15 14:05:25 +0700

    [FIX] Error handler for duplicated email Penanganan eror saat membuat user baru dengan email yang sudah ada di DB

--- services/users.js
+++ services/users.js
@@ -1,6 +1,10 @@
 import bcrypt from 'bcrypt';
+import dotenv from 'dotenv';
+import jwt from 'jsonwebtoken';
 import * as userRepo from '../repositories/users.js';
 import { successResponse, errorResponse } from '../utils/response.js';
+
+dotenv.config();
 
 const getUsers = async (req, res, next) => {
   try {
@@ -16,12 +20,22 @@ const createUser = async (req, res, next) => {
   try {
     const { email, password } = req.body;
     bcrypt.hash(password, 10, async (error, hashedPassword) => {
-      const [result] = await userRepo.createUser(email, hashedPassword);
-      const payload = { id: result.insertId, email };
-      successResponse(res, 'User created successfully', payload, 201);
+      try {
+        const [result] = await userRepo.createUser(email, hashedPassword);
+        const payload = { id: result.insertId, email };
+        successResponse(res, 'User created successfully', payload, 201);
+      } catch (error) {
+        if (error.code === 'ER_DUP_ENTRY') {
+          // Ini adalah kode kesalahan yang mengindikasikan email sudah ada (unik)
+          errorResponse(res, 'Email is already in use', 409);
+        } else {
+          // Penanganan kesalahan lainnya
+          throw error;
+        }
+      }
     });
   } catch (error) {
-    errorResponse(res, 'User creation failed', 409);
+    errorResponse(res, 'User creation failed', 500);
     next(error);
   }
 };
@@ -53,5 +67,30 @@ const updateUser = async (req, res, next) => {
     next(error);
   }
 };
+
+// const login = async (req, res, next) => {
+//   try {
+//     const { email, password } = req.body;
+//     const [user] = await userRepo.getUserByEmail(email);
+//     if (user.length === 0) throw new Error('User not found');
+//     bcrypt.compare(password, user.password, async (error, result) => {
+//       console.log(result);
+//       if (!result) throw new Error('Incorrect password');
+//     });
+//     const payload = { id: user.id, email: user.email };
+//     const accessToken = jwt.sign(payload, process.env.SECRET_ACCESS_TOKEN, {
+//       expiresIn: '1h',
+//     });
+//     const refreshToken = jwt.sign(payload, process.env.SECRET_REFRESH_TOKEN, {
+//       expiresIn: '8h',
+//     });
+//     const token = { accessToken, refreshToken };
+//     successResponse(res, 'User login successfully', token);
+//   } catch (error) {
+//     errorResponse(res, error.message || 'User login failed', 401);
+//     next(error);
+//     return;
+//   }
+// };
 
 export { getUsers, createUser, deleteUser, updateUser };
